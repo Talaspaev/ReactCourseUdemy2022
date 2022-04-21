4c66e5aa81fa47b4e39451f76907630c9274870e
mukhamed.talaspaev.epam <mukhamed_talaspaev@epam.com>
2022-04-22 00:13:15 +0300

    convert HTML to jsx

--- src/index.js
+++ src/index.js
@@ -6,7 +6,14 @@ import ReactDOM from "react-dom";
 // Create JSX component
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <label for="name">Enter name:</label>
+      <input id="name" />
+      // eslint-disable-next-line react/style-prop-object
+      <button style="background:red;">Submit</button>
+    </div>
+  );
 };
 
 //Take react component and show it on the screen
